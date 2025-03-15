4ddac195880f72ee40c60b8e7f0242c54f18bb3e
HkhCode <93916973+HkhCode@users.noreply.github.com>
2025-03-15 07:59:38 -0700

    init

---
+++ Components/Login.jsx
@@ -0,0 +1,120 @@
+import "react";
+import styles from "../src/styles/Login.module.css";
+import React, { useState } from "react";
+import { Box, Button, useTheme } from "@mui/material";
+import { ChevronLeft, ChevronRight } from "@mui/icons-material";
+import CircleIcon from '@mui/icons-material/Circle';
+import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
+const slides = [
+  {
+    id: 1,
+    title: "همکاری",
+    desc: "با دوستان و همکاران خود همکاری داشته باشید و به طور همزمان بر روی یک سند نگارش انجام دهید.",
+  },
+  {
+    id: 2,
+    title: "همکاری",
+    desc: "با دوستان و همکاران خود همکاری داشته باشید و به طور همزمان بر روی یک سند نگارش انجام دهید.",
+  },
+  {
+    id: 3,
+    title: "همکاری",
+    desc: "با دوستان و همکاران خود همکاری داشته باشید و به طور همزمان بر روی یک سند نگارش انجام دهید.",
+  },
+  {
+    id: 4,
+    title: "همکاری",
+    desc: "با دوستان و همکاران خود همکاری داشته باشید و به طور همزمان بر روی یک سند نگارش انجام دهید.",
+  },
+];
+const Login = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const theme = useTheme();
+
+  const handleFirst = (e) => {
+    console.log(currentIndex);
+    setCurrentIndex(1);
+  };
+
+  const handleSecond = (e) => {
+    console.log(currentIndex);
+    setCurrentIndex(2);
+  };
+  const handleThird = (e) => {
+    console.log(currentIndex);
+    setCurrentIndex(3);
+  };
+  const handleFourth = (e) => {
+    console.log(currentIndex);
+    setCurrentIndex(4);
+  };
+  return (
+    <div className={styles.Bakcground}>
+      <div className={styles.Box}>
+        <div className={styles.InnerBox}>
+          <div className={styles.detailsContainer}>
+            <span className={styles.Title}>ایران سند</span>
+            <div className={styles.ImageContainer}>
+              <img
+                src="../src/Images/b8fce91ad812f4581b7eebab2147575e.png"
+                className={styles.image}
+              />
+            </div>
+            <div className={styles.InformationContainer}>
+              <Box
+                sx={{
+                  position: "relative",
+                  width: "100%",
+                  maxWidth: 800,
+                  margin: "0 auto",
+                  overflow: "hidden",
+                }}
+              >
+                <Box
+                  sx={{
+                    display: "flex",
+                    transition: "transform 0.5s ease-in-out",
+                    transform: `translateX(-${currentIndex * 100}%)`,
+                  }}
+                >
+                  {slides.map((slide) => (
+                    <Box
+                      key={slide.id}
+                      sx={{
+                        minWidth: "100%",
+                        boxSizing: "border-box",
+                        height: "40%"
+                      }}
+                    >
+                      <span className={styles.infoTitle}>{slide.title}</span>
+                      <br />
+                      <p className={styles.infoContent}>{slide.desc}</p>
+                    </Box>
+                  ))}
+                </Box>
+                <div className={styles.sliderBtns}>
+                  <CircleIcon onClick={handleFirst} fontSize="small" sx={{
+                      color:"#B2B2B2"
+                    }}/>
+                  <CircleIcon onClick={handleSecond} fontSize="small" sx={{
+                      color:"#B2B2B2"
+                    }}/>
+                  <CircleIcon onClick={handleThird} fontSize="small" sx={{
+                      color:"#FFFFFF"
+                    }}/>
+                  <CircleIcon onClick={handleFourth} fontSize="small" sx={{
+                      color:"#B2B2B2"
+                    }}/>
+                </div>
+
+              </Box>
+            </div>
+          </div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import { createRoot } from 'react-dom/client'
+import Login from '../Components/Login.jsx'
+import '../src/index.css';
+createRoot(document.getElementById('root')).render(
+    <Login />,
+)
