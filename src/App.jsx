87faaaeff7705493023c0b954c9ca83f0644ef61
HkhCode <93916973+HkhCode@users.noreply.github.com>
2025-03-21 12:27:31 -0700

    loading added adjustments to signup

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,35 @@ import Login from "../Components/Login";
 import Forgot_password from "../Components/Forgot_password";
 import UserDashboard from "../Components/UserDashboard/user_dashboard";
 import "../Styles/App.css";
+import { useState , useEffect } from "react";
+import Loading from '../Components/Loading';
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          <Route index element={<UserDashboard />} />
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<SignUp />} />
-          <Route path="forgot_password" element={<Forgot_password />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/">
+              <Route path="login" element={<Login />} />
+              <Route path="signup" element={<SignUp />} />
+              <Route path="forgot_password" element={<Forgot_password />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      )}
+    </>
   );
 }
