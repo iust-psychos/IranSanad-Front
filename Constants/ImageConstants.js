0dd1724a5b634fa306ca1ad62aa0d0a8051c4454
HkhCode <93916973+HkhCode@users.noreply.github.com>
2025-05-30 12:50:13 +0200

    Constatns added

---
+++ Constants/ImageConstants.js
@@ -0,0 +1,16 @@
+export const slideImage1 = "./slideImages/slideImage1.svg";
+export const slideImage2 = "./slideImages/slideImage2.svg";
+export const slideImage3 = "./slideImages/slideImage3.svg";
+export const slideImage4 = "./slideImages/slideImage4.svg";
+
+export const DEFAULT_IMAGE = "@/public/UserProfile/Default.png";
+export const userProfileIcon = "@/public/user-profile.png";
+
+export const Service1 = "@/public/LandingPage/service1.svg";
+export const Service2 = "@/public/LandingPage/service2.svg";
+export const Service3 = "@/public/LandingPage/service3.svg";
+export const Service4 = "@/public/LandingPage/service4.svg";
+
+export const Join = "@/public/LandingPage/join.svg";
+export const logo_dark = "/logo_dark.png";
+export const logo_light = "/logo_light.png";
