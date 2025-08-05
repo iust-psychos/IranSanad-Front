fe325bffd156e03dadeaf2549077931f46276e26
Erfan Hemati <efnhmt2020@gmail.com>
2025-08-05 21:47:39 +0330

    Voice2Text Module Updated & Connected to Server

--- pages/ContentEdit/VoiceText.jsx
+++ pages/ContentEdit/VoiceText.jsx
@@ -11,6 +11,7 @@ const VoiceText = () => {
   const token = CookieManager.LoadToken();
 
   const [audioFile, setAudioFile] = useState(null);
+  const [recordedAudioBlob, setRecordedAudioBlob] = useState(null);
   const [transcription, setTranscription] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const fileInputRef = useRef(null);
@@ -21,15 +22,29 @@ const VoiceText = () => {
     console.log(file);
     if (file && file.type.includes("audio")) {
       setAudioFile(file);
-      console.log("in: ", audioFile);
+      setRecordedAudioBlob(null);
     } else {
       showErrorToast("لطفاً یک فایل صوتی معتبر انتخاب کنید");
       setAudioFile(null);
     }
   };
 
+  const handleChangeRecordedAudio = (audioBlob) => {
+    setRecordedAudioBlob(audioBlob);
+    setAudioFile(null);
+  };
+
   const convertVoice2Text = async () => {
-    if (!audioFile) {
+    let fileToSend = audioFile;
+
+    if (voiceMode === "record" && recordedAudioBlob) {
+      fileToSend = new File([recordedAudioBlob], "recording.wav", {
+        type: "audio/wav",
+      });
+    }
+
+    if (!fileToSend) {
+      showErrorToast("لطفاً ابتدا یک فایل صوتی انتخاب یا ضبط کنید");
       return;
     }
 
@@ -37,7 +52,7 @@ const VoiceText = () => {
 
     try {
       const formData = new FormData();
-      formData.append("audio_file", audioFile);
+      formData.append("audio_file", fileToSend);
 
       const response = await axios.post(voiceTextAPI, formData, {
         headers: {
@@ -48,6 +63,7 @@ const VoiceText = () => {
       setTranscription(response.data.transcription);
     } catch (error) {
       showErrorToast("خطا در پردازش فایل صوتی!");
+      console.error(error);
     } finally {
       setIsLoading(false);
     }
@@ -64,38 +80,71 @@ const VoiceText = () => {
       });
   };
 
+  const [voiceMode, setVoiceMode] = useState("file");
+  const handleChangeVoiceMode = (value) => {
+    setVoiceMode(value);
+    setAudioFile(null);
+    setRecordedAudioBlob(null);
+  };
+
   return (
     <div className="SideTool-Voice-Container">
       <h2 className="SideTool-Voice-Header SideTool-Voice-Item">
         تبدیل صوت به متن
       </h2>
-      <p className="SideTool-Voice-Text SideTool-Voice-Item">
-        فایل صوتی را در این قسمت بارگذاری کنید و متن آن را دریافت کنید.
-      </p>
-      <input
-        ref={fileInputRef}
-        type="file"
-        accept="audio/*"
-        style={{ display: "none" }}
-        name="SideTool-Voice"
-        id="SideTool-Voice"
-        onChange={handleFileChange}
-      />
-      <label htmlFor="SideTool-Voice">
+      <div className="SideTool-Voice-Type-Selector">
         <button
-          className="SideTool-Voice-File SideTool-Voice-Item"
-          type="button"
-          onClick={() => document.getElementById("SideTool-Voice").click()}
+          className={voiceMode === "file" ? "activeSelector" : ""}
+          onClick={() => {
+            handleChangeVoiceMode("file");
+          }}
         >
-          {audioFile ? audioFile.name : <FiUpload />}
+          بارگذاری صوت
         </button>
-      </label>
+        <button
+          className={voiceMode === "record" ? "activeSelector" : ""}
+          onClick={() => {
+            handleChangeVoiceMode("record");
+          }}
+        >
+          ضبط صوت
+        </button>
+      </div>
+      <p className="SideTool-Voice-Text SideTool-Voice-Item">
+        {voiceMode === "file"
+          ? "فایل صوتی را بارگذاری کنید و متن آن را دریافت کنید."
+          : "فایل صوتی را ضبط کرده و متن آن را دریافت کنید."}
+      </p>
+      {voiceMode === "file" ? (
+        <>
+          <input
+            ref={fileInputRef}
+            type="file"
+            accept="audio/*"
+            style={{ display: "none" }}
+            name="SideTool-Voice"
+            id="SideTool-Voice"
+            onChange={handleFileChange}
+          />
+          <label htmlFor="SideTool-Voice">
+            <button
+              className="SideTool-Voice-File SideTool-Voice-Item"
+              type="button"
+              onClick={() => document.getElementById("SideTool-Voice").click()}
+            >
+              {audioFile ? audioFile.name : <FiUpload />}
+            </button>
+          </label>{" "}
+        </>
+      ) : (
+        <VoiceRecorder onRecordingComplete={handleChangeRecordedAudio} />
+      )}
 
       <button
         className="SideTool-Voice-Button SideTool-Voice-Item"
         type="submit"
         onClick={convertVoice2Text}
-        disabled={!audioFile || isLoading}
+        disabled={(!audioFile && !recordedAudioBlob) || isLoading}
       >
         {isLoading ? "در حال پردازش..." : "تبدیل"}
       </button>
@@ -121,19 +170,6 @@ const VoiceText = () => {
           </button>{" "}
         </>
       )}
-
-      <p className="SideTool-Voice-Text SideTool-Voice-Item">
-        به صورت زنده فایل صوتی را در این قسمت ضبط کرده و متن آن را دریافت کنید.
-      </p>
-      <VoiceRecorder />
-      <button
-        className="SideTool-Voice-Button SideTool-Voice-Item"
-        type="submit"
-        onClick={convertVoice2Text}
-        disabled={!audioFile || isLoading}
-      >
-        {isLoading ? "در حال پردازش..." : "تبدیل"}
-      </button>
     </div>
   );
 };
